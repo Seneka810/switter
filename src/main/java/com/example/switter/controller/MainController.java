65b76696d535eb390ffa907af8e50807a89120a3
unknown <kordyk.stanislav@gmail.com>
2020-03-22 21:24:31 +0200

    release v1

---
+++ src/main/java/com/example/switter/controller/MainController.java
@@ -0,0 +1,75 @@
+package com.example.switter.controller;
+
+import com.example.switter.domain.Message;
+import com.example.switter.domain.User;
+import com.example.switter.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+
+@Controller
+public class MainController {
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @Value("${upload.path}")
+    private String uploadPath;
+
+    @GetMapping("/")
+    public String greeting(Map<String, Object> model) {
+        return "greeting";
+    }
+
+    @GetMapping("/main")
+    public String main(@RequestParam(required = false, defaultValue = "") String tag, Model model){
+        Iterable<Message> messages;
+        if(tag !=null && !tag.isEmpty()){
+            messages = messageRepo.findByTag(tag);
+        } else {
+            messages = messageRepo.findAll();
+        }
+        model.addAttribute("messages", messages);
+        model.addAttribute("filter", tag);
+        return "main";
+    }
+
+    @PostMapping("/main")
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag, Map<String, Object> model,
+            @RequestParam("file") MultipartFile file) throws IOException {
+        Message message = new Message(text, tag, user);
+
+        if(file != null && !file.getOriginalFilename().isEmpty()) {
+            File uploadDir = new File(uploadPath);
+
+            if(!uploadDir.exists()) {
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFileName = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFileName));
+            message.setFilename(resultFileName);
+        }
+
+        messageRepo.save(message);
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+}
---
+++ src/main/java/com/example/switter/repos/MessageRepo.java
@@ -0,0 +1,11 @@
+package com.example.switter.repos;
+
+import com.example.switter.domain.Message;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MessageRepo extends CrudRepository<Message, Integer> {
+
+    List<Message> findByTag(String tag);
+}
